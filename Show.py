e19f59c65e6568dd915d102f977ac17d787f26a9
Suhas Hariharan <hariharansuhas@gmail.com>
2022-08-04 21:26:28 +0800

    n

--- Show.py
+++ Show.py
@@ -1,4 +1,5 @@
 import requests
+from seating import SeatingPlan, Seat, GVSeatingPlan
 
 
 class Show:
@@ -36,7 +37,17 @@ class Show:
 
 
 class GVShow(Show):
+    # Seat Status:
+    # L - seat available
+    # B - seat booked
+    # T - seat blocked
+
+    # Seat Types:
+    # W - wheelchair bearth
+    # N - normal seat
     GV_SEATING_PLAN_URL = "https://www.gv.com.sg/.gv-api/seatplan"
+
+
 
     def __init__(self, movie, theatre, theatre_chain, has_subtitles, subtitles_language, show_date, timezone, start_time, rating, hall, show_url, cinema_id, film_code):
         self.cinema_id = cinema_id
@@ -54,5 +65,13 @@ class GVShow(Show):
             'showTime': show_time,
             'hallNumber': hall_number,
         }
-        seating_plan = requests.post(self.GV_SEATING_PLAN_URL, json=json_data).json()
-
+        seating_data = requests.post(
+            self.GV_SEATING_PLAN_URL, json=json_data).json()['data']
+        seating_plan = GVSeatingPlan([])
+        for row in seating_data:
+            seating_plan.seat_matrix.append([])
+            for col in row:
+                seat_status = self.get_gv_seat_status(col['status'])
+                seat_type = self.get_gv_seat_type(col['type'])
+                seating_plan.seat_matrix[seating_plan.row_count(
+                ) - 1].append(Seat(col['rowNumber'], col['colNumber'], seat_status, seat_type))
--- seating.py
+++ seating.py
@@ -1,15 +1,51 @@
 class SeatingPlan:
     # 0 - seat not available
     # 1 - seat taken
-    # 2 - seat available
+    # 2 - seat temporarily blocked
+    # 3 - seat available
+
     def __init__(self, seat_matrix):
         self.seat_matrix = seat_matrix
+
+    def row_count(self):
+        return len(self.seat_matrix)
 
     def is_seat_available(self, row, col):
         return self.seat_matrix[row][col].status == 2
 
+class GVSeatingPlan(SeatingPlan):
+    AVAILABLE_STATUSES = ['L']
+    TEMP_UNAVAILABLE_STATUSES = ['T']
+    UNAVAILABLE_STATUSES = ['B']
+
+    ACCESSIBLE_TYPE = ['W']
+    NORMAL_TYPE = ['N']
+
+    def __init__(self, seat_matrix):
+        SeatingPlan(seat_matrix)
+
+    def get_gv_seat_status(self, status):
+        if status in self.AVAILABLE_STATUSES:
+            return 2
+        elif status in self.TEMP_UNAVAILABLE_STATUSES:
+            return 1
+        elif status in self.UNAVAILABLE_STATUSES:
+            return 0
+
+    def get_gv_seat_type(self, seat_type):
+        if seat_type in self.ACCESSIBLE_TYPE:
+            return 0
+        elif seat_type in self.NORMAL_TYPE:
+            return 1
+
+
+
 class Seat:
-    def __init__(self, row, col, status):
+    def __init__(self, row, col, status, seat_type):
         self.row = row
         self.col = col
         self.status = status
+        self.seat_type = seat_type
+
+
+
